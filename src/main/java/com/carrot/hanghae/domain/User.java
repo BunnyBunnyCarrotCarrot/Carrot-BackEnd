fbaa1a4ce156d6c8d74881f5250a28d882aaa728
hobit22 <hobit22@naver.com>
2022-04-15 19:35:16 +0900

    * User domain Setting Timestamped 추가

--- src/main/java/com/carrot/hanghae/domain/User.java
+++ src/main/java/com/carrot/hanghae/domain/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -14,4 +15,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false)
+    private String userPw;
+
+    public User (String userId, String userPw){
+        this.userId = userId;
+        this.userPw = userPw;
+
+    }
 }
