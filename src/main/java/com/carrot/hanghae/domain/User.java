d9d0bb8e0f46050479c4fa6a3ff005fb666d036d
hobit22 <hobit22@naver.com>
2022-04-16 13:59:02 +0900

    * Test input 주석 * Todo 유효성 검사

--- src/main/java/com/carrot/hanghae/domain/User.java
+++ src/main/java/com/carrot/hanghae/domain/User.java
@@ -1,12 +1,10 @@
 package com.carrot.hanghae.domain;
 
-import com.carrot.hanghae.dto.UserRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
