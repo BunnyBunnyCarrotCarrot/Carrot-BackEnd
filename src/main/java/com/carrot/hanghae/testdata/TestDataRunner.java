c46a92748108dc5ed907b64aebde8bfaadf005d7
journeypark93 <wwwsomty@gmail.com>
2022-04-20 20:56:14 +0900

    test

--- src/main/java/com/carrot/hanghae/testdata/TestDataRunner.java
+++ src/main/java/com/carrot/hanghae/testdata/TestDataRunner.java
@@ -1,52 +1,44 @@
-package com.carrot.hanghae.testdata;
-
-
-import com.carrot.hanghae.domain.Location;
-import com.carrot.hanghae.repository.LocationRepository;
-import com.carrot.hanghae.repository.UserRepository;
-import com.carrot.hanghae.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-public class TestDataRunner implements ApplicationRunner {
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @Autowired
-    LocationRepository locationRepository;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-// 테스트 User 생성
-        // 테스트 User 의 관심상품 등록
-        // 검색어 당 관심상품 10개 등록
-//        createTestData(1L, "서울");
-//        createTestData(2L, "인천");
-//        createTestData(3L, "부산");
-//        createTestData(4L, "경기");
-//        Location location = locationRepository.findById(1L).orElseThrow(
-//                () -> new IllegalArgumentException("잘못된 접근입니다.")
-//        );
-//        User testUser = new User("슈가", passwordEncoder.encode("123"), location);
-//        testUser = userRepository.save(testUser);
-
-    }
-
-    private void createTestData(Long locationId, String locationName) throws IOException {
-        Location location = new Location(locationId, locationName);
-        locationRepository.save(location);
-    }
-}
+//package com.carrot.hanghae.testData;
+//
+//import com.carrot.hanghae.domain.Category;
+//import com.carrot.hanghae.domain.Location;
+//import com.carrot.hanghae.domain.User;
+//import com.carrot.hanghae.repository.CategoryRepository;
+//import com.carrot.hanghae.repository.LocationRepository;
+//import com.carrot.hanghae.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class testDataRunner implements ApplicationRunner {
+//
+//
+//    @Autowired
+//    private UserRepository userRepository;
+////    @Autowired
+////    private PasswordEncoder passwordEncoder;
+//    @Autowired
+//    private CategoryRepository categoryRepository;
+//    @Autowired
+//    private LocationRepository locationRepository;
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//        // 테스트 User 생성
+//        Location location = new Location(11L,"서울특별시");
+//        locationRepository.save(location);
+//        User user0= new User("test","닉네임입니다", "1234", location);
+//        User user1= new User("test2","닉네임입니다2", "12345", location);
+//        System.out.println("유저를 저장합니다.");
+//        userRepository.save(user0);
+//        userRepository.save(user1);
+//
+//        Category category1 = new Category(1L, "디지털기기");
+//        Category category2 = new Category(2L, "주방용품");
+//        System.out.println("카테고리를 저장합니다.");
+//        categoryRepository.save(category1);
+//        categoryRepository.save(category2);
+//    }
+//}
